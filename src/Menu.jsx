f3ff8a53c16d98414a165f8936b4302bc8cb1491
Noxx-design <rm94529@estudante.fieb.edu.br>
2025-06-27 11:28:18 +0000

    Menu2.0

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -6,13 +6,13 @@ const Menu = () => {
         <div className="Menu">
             <ul className="nav">
                 <li className="nav-item">
-                    <a className="nav-link" href="#">Item do Menu</a>
+                    <a className="nav-link" href="/">Item do Menu</a>
                 </li>
                 <li className="nav-item">
                     <a className="nav-link" href="/home">Home</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="/pokeflex">pokeFlex</a>
+                    <a className="nav-link" href="/pokeflex">PokeFlex</a>
                 </li>
                 <li className="nav-item">
                     <a className="nav-link" href="/pokegrid">PokeGrid</a>
